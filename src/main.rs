008f828638286be5b1b9e77eaebeb86f33d6bbbb
cammatsui <cmatsui11@gmail.com>
2022-09-16 23:23:34 -0400

    Update recorders

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use aqt_sim::network::presets;
 use aqt_sim::protocol::Protocol;
 use aqt_sim::adversary::Adversary;
 use aqt_sim::simulation::threshold::TimedThreshold;
-use aqt_sim::simulation::recorder::{ Recorder, DebugPrintRecorder, BufferLoadRecorder, AbsorptionRecorder };
+use aqt_sim::simulation::recorder::{ Recorder, DebugPrintRecorder, FileRecorder, FileRecorderType };
 use aqt_sim::adversary::path_random::SDPathRandomAdversary;
 
 const NUM_BUFFERS: usize = 10;
@@ -17,8 +17,8 @@ fn main() {
     let threshold = TimedThreshold::new(NUM_RDS);
     let recorders: Vec<Box<dyn Recorder>> = vec![
         Box::new(DebugPrintRecorder::new()),
-        Box::new(BufferLoadRecorder::new()),
-        Box::new(AbsorptionRecorder::new()),
+        Box::new(FileRecorder::new(FileRecorderType::BufferLoadCSV)),
+        Box::new(FileRecorder::new(FileRecorderType::AbsorptionCSV)),
     ];
     let mut simulation = Simulation::new(network, protocol, adversary, threshold, recorders, "/home/cammatsui/Dev/aqt_sim/output".to_string());
     simulation.run();
--- src/simulation/recorder.rs
+++ src/simulation/recorder.rs
@@ -1,12 +1,10 @@
-//! This module contains the `Recorder` trait and its implementations, which takes "snapshots" of
-//! the simulation/network and either prints them to the console or writes them to a file.
-
 use std::fs;
 use std::io::prelude::*;
 use crate::network::Network;
 use crate::packet::Packet;
 
-const DEFAULT_LINE_LIMIT: usize = 5000;
+// For CSV/file writing, how many lines to keep in memory before writing to disk.
+const LINE_LIMIT: usize = 5000;
 
 
 /// Trait implemented by all recorders.
@@ -17,104 +15,119 @@ pub trait Recorder {
 }
 
 
-/// Trait implemented by all recorders which save to a file.
-pub trait SaveRecorder {
-    /// If the line limit has been reached, saves all of the recorder's lines to the file at the
-    /// file path. Otherwise, just appends the line to lines.
-    fn write(&mut self, line: String, filename: &str) {
-        if self.get_lines().len() >= self.get_line_limit() {
-            // Write to file
-            self.save(filename);
-            self.reset_lines();
-        }
-        self.push_line(line);
-    }
-
-    /// Append the `SaveRecorder`'s lines to the file at the recorder's output path.
-    fn save(&mut self, filename: &str) {
-        let data = self.get_lines().concat();
-
-        let output_path_unwrapped = self.get_output_path()
-            .expect("You must set an output path for each recorder.");
-        let mut path = String::from(output_path_unwrapped);
-
-        fs::create_dir_all(path.clone())
-            .expect("Failed to save simulation results.");
-        path.push('/');
-        path.push_str(filename);
-
-        let mut file = fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .append(true)
-            .open(&path)
-            .unwrap();
-
-        if let Err(e) = writeln!(file, "{}", data) {
-            eprintln!("Couldn't write to file: {}", e);
-        }
-    }
-
-    /// Get the line vector.
-    fn get_lines(&self) -> &Vec<String>;
-
-    /// Push a line to the line vector.
-    fn push_line(&mut self, line: String);
-
-    /// Reset the line vector to a new empty vector.
-    fn reset_lines(&mut self);
-
-    /// Get the line limit, the number of lines to keep in memory before writing to disk.
-    fn get_line_limit(&self) -> usize;
-
-    // Get the output path which we save the results to.
-    fn get_output_path(&self) -> Option<&String>;
-}
-
-
-/// Prints the whole network to the console each timestep.
+/// Prints the network and any to the console.
 pub struct DebugPrintRecorder;
 
 impl DebugPrintRecorder {
-    /// Get a new `DebugPrintRecorder`.
-    pub fn new() -> Self {
-        DebugPrintRecorder {}
-    }
+    pub fn new() -> Self { DebugPrintRecorder}
 }
 
 impl Recorder for DebugPrintRecorder {
-    /// Does nothing since this `Recorder` does not save anything.
-    fn set_output_path(&mut self, _output_path: String) { }
-
     fn record(
         &mut self,
         rd: usize,
         prime: bool,
         network: &Network,
-        _absorbed: Option<&Vec<Packet>>,
+        absorbed: Option<&Vec<Packet>>,
     ) {
         if prime { println!("{}':", rd) } else { println!("{}:", rd) };
         println!("{}", network);
+        if let Some(absorbed_packets) = absorbed {
+            if absorbed_packets.len() == 0 { return };
+            println!("Absorbed Packets:");
+            for packet in absorbed_packets {
+                println!("{:?}", packet);
+            }
+        }
+        println!();
     }
 
     fn close(&mut self) {
         println!("Simulation finished.");
     }
+
+    fn set_output_path(&mut self, _output_path: String) { }
 }
 
 
-const BUFFER_LOAD_CSV_HEADER: &str = "rd,prime,buffer_from,buffer_to,load\n";
-/// Records the load in each buffer and saves to a csv.
-pub struct BufferLoadRecorder {
-    line_limit: usize, // How many lines to keep in memory before dumping record to file. 
+/// Types of file recorders.
+#[derive(Clone, Copy)]
+pub enum FileRecorderType {
+    AbsorptionCSV,
+    BufferLoadCSV,
+}
+
+
+/// Write some aspect of the simulation state to a file.
+pub struct FileRecorder {
+    recorder_type: FileRecorderType,
     lines: Vec<String>,
-    output_path: Option<String>,
-    filename: &'static str,
+    // We require the output dir path to be set; optional so that Simulation::new() caller doesn't
+    // have to construct and provide every individual file's output path.
+    file_path: Option<String>,
 }
 
-impl Recorder for BufferLoadRecorder {
-    fn set_output_path(&mut self, output_path: String) {
-       self.output_path = Some(output_path); 
+impl FileRecorder {
+    pub fn new(recorder_type: FileRecorderType) -> Self {
+        FileRecorder {
+            recorder_type,
+            lines: vec![Self::type_to_header(recorder_type).to_string()],
+            file_path: None,
+        }
+    }
+
+    const fn type_to_filename(recorder_type: FileRecorderType) -> &'static str {
+        match recorder_type {
+            FileRecorderType::AbsorptionCSV => "absorption.csv",
+            FileRecorderType::BufferLoadCSV => "buffer_load.csv",
+        }
+    }
+
+    const fn type_to_header(recorder_type: FileRecorderType) -> &'static str {
+        match recorder_type {
+            FileRecorderType::AbsorptionCSV => "rd,packet_id,packet_injection_rd\n",
+            FileRecorderType::BufferLoadCSV => "rd,prime,buffer_from,buffer_to,load\n",
+        }
+    }
+
+    /// Write a line to the recorder.
+    pub fn write(&mut self, line: String) {
+        if self.lines.len() >= LINE_LIMIT {
+            self.save();
+            self.lines = Vec::new();
+        }
+        self.lines.push(line);
+    }
+
+    /// Save the lines to a file.
+    pub fn save(&mut self) {
+        let data = self.lines.concat();
+        let file_path_unwrapped = self.file_path.as_ref()
+            .expect("You must set an output path for each recorder.");
+
+        let mut file = fs::OpenOptions::new()
+            .write(true).create(true).append(true).open(&file_path_unwrapped)
+            .expect(&format!("Failed to save simulation results to {}", file_path_unwrapped));
+
+        if let Err(_) = writeln!(file, "{}", data) {
+            eprintln!("Failed to save simulation results to {}", file_path_unwrapped);
+        }
+    }
+}
+
+impl Recorder for FileRecorder {
+    fn close(&mut self) {
+        self.save();
+    }
+
+    fn set_output_path(&mut self, dir_path: String) {
+        fs::create_dir_all(dir_path.clone())
+            .expect(&format!("Failed to save simulation results to {}", &dir_path));
+
+        let mut file_path = String::from(dir_path);
+        file_path.push('/');
+        file_path.push_str(Self::type_to_filename(self.recorder_type));
+        self.file_path = Some(file_path);
     }
 
     fn record(
@@ -122,134 +135,29 @@ impl Recorder for BufferLoadRecorder {
         rd: usize,
         prime: bool,
         network: &Network,
-        _absorbed: Option<&Vec<Packet>>,
-    ) {
-        // format: rd,prime (0/1), buffer_from, buffer_to, load (see BUFFER_LOAD_CSV_HEADER)
-        let prime_flag = if prime { 1 } else { 0 };
-        for (from_id, to_id) in network.get_edgebuffers() {
-            let load = network.get_edgebuffer(from_id, to_id).unwrap().buffer.len();
-            self.write(
-                format!("{},{},{},{},{}\n", rd, prime_flag, from_id, to_id, load),
-                self.filename
-            );
-        }
-    }
-
-    fn close(&mut self) {
-        self.save(self.filename);
-    }
-}
-
-impl SaveRecorder for BufferLoadRecorder {
-    fn get_lines(&self) -> &Vec<String> {
-        &self.lines
-    }
-
-    fn push_line(&mut self, line: String) {
-        self.lines.push(line);
-    }
-
-    fn reset_lines(&mut self) {
-        self.lines = Vec::new();
-    }
-
-    fn get_line_limit(&self) -> usize {
-        self.line_limit
-    }
-
-    fn get_output_path(&self) -> Option<&String> {
-        self.output_path.as_ref()
-    }
-}
-
-impl BufferLoadRecorder {
-    /// Get a new `BufferLoadRecorder` with the default line limit.
-    pub fn new() -> Self {
-        Self::new_with_line_limit(DEFAULT_LINE_LIMIT)
-    }
-
-    /// Get a new `BufferLoadRecorder` with the specified line limit.
-    pub fn new_with_line_limit(local_line_limit: usize) -> Self {
-        BufferLoadRecorder {
-            line_limit: local_line_limit,
-            lines: vec![BUFFER_LOAD_CSV_HEADER.to_string()],
-            output_path: None,
-            filename: "buffer_load.csv",
-        }
-    }
-}
-
-
-const ABSORPTION_CSV_HEADER: &str = "rd,packet_id,packet_injection_rd\n";
-/// Records round number, injection round, packet ID of each absorbed `Packet`.
-pub struct AbsorptionRecorder {
-    line_limit: usize, // How many lines to keep in memory before dumping record to file. 
-    lines: Vec<String>,
-    output_path: Option<String>,
-    filename: &'static str,
-}
-
-impl Recorder for AbsorptionRecorder {
-    fn record(
-        &mut self,
-        rd: usize,
-        prime: bool,
-        _network: &Network,
         absorbed: Option<&Vec<Packet>>,
     ) {
-        if !prime { return };
-        for packet in absorbed.unwrap() {
-            self.write(
-                format!("{},{},{}\n", rd, packet.get_id(), packet.get_injection_rd()),
-                self.filename
-            );
-        }
-    }
-
-    fn set_output_path(&mut self, output_path: String) {
-        self.output_path = Some(output_path);
-    }
-
-    fn close(&mut self) {
-        self.save(self.filename);
-    }
-}
-
-impl SaveRecorder for AbsorptionRecorder {
-    fn get_lines(&self) -> &Vec<String> {
-        &self.lines
-    }
-
-    fn push_line(&mut self, line: String) {
-        self.lines.push(line);
-    }
-
-    fn reset_lines(&mut self) {
-        self.lines = Vec::new();
-    }
-
-    fn get_line_limit(&self) -> usize {
-        self.line_limit
-    }
-
-    fn get_output_path(&self) -> Option<&String> {
-        self.output_path.as_ref()
-    }
-}
-
-impl AbsorptionRecorder {
-    /// Get a new `AbsorptionLoadRecorder` with the default line limit.
-    pub fn new() -> Self {
-        Self::new_with_line_limit(DEFAULT_LINE_LIMIT)
-    }
-
-    /// Get a new `AbsorptionLoadRecorder` with the specified line limit.
-    pub fn new_with_line_limit(local_line_limit: usize) -> Self {
-        AbsorptionRecorder {
-            line_limit: local_line_limit,
-            lines: vec![ABSORPTION_CSV_HEADER.to_string()],
-            output_path: None,
-            filename: "absorption.csv",
+        match self.recorder_type {
+            FileRecorderType::AbsorptionCSV => {
+                if !prime { return };
+                for packet in absorbed.unwrap() {
+                    self.write(format!(
+                        "{},{},{}\n",
+                        rd,
+                        packet.get_id(),
+                        packet.get_injection_rd()
+                    ));
+                }
+            },
+            FileRecorderType::BufferLoadCSV => {
+                let prime_flag = if prime { 1 } else { 0 };
+                for (from_id, to_id) in network.get_edgebuffers() {
+                    let load = network.get_edgebuffer(from_id, to_id).unwrap().buffer.len();
+                    self.write(
+                        format!("{},{},{},{},{}\n", rd, prime_flag, from_id, to_id, load),
+                    );
+                }
+            }
         }
     }
 }
